660420698b3786e54f684b41fe943d795d8407c9
rmccull4 <rmccull4@gmail.com>
2018-09-05 04:58:19 +0000

    Running Tasks with Grunt

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-    <script src="js/scripts.js"></script>
+$(document).ready(function() {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+    $("#login-button").click(function() {
+        $("#loginModal").modal("show");
+    });
+    $("#registration-button").click(function() {
+        $("#reservationModal").modal("show");
+    });
+});
